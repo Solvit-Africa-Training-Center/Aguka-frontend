1e375a61d117bad5d62dfce487133745c166be07
patricknambajimana <patricknambajimana6@gmail.com>
2025-09-03 13:34:33 +0200

    servicepage is ready

--- src/pages/ServicePage.tsx
+++ src/pages/ServicePage.tsx
@@ -1,5 +1,12 @@
 import { FaUserFriends } from "react-icons/fa";
 import { GiShakingHands } from "react-icons/gi";
+import { SlChart } from "react-icons/sl";
+import { IoCard } from "react-icons/io5";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { AiFillThunderbolt } from "react-icons/ai";
+import { VscBook } from "react-icons/vsc";
+import { SquareCheckBig } from "lucide-react";
+
 function ServicePage() {
   return (
     <div className="w-full  font-poppins bg-[#003B42] text-white">
@@ -68,7 +75,7 @@ function ServicePage() {
         <div className="w-full h-[388px]">
           <div className="grid grid-cols-2 gap-10 pt-10 ">
             <div className="w-[646px] h-[388px] grid justify-between  text-left">
-              <FaUserFriends className="size-15 text-secondary-300" />
+              <IoCard className="size-15 text-secondary-300" />
               <h1 className="text-[#F4F4F4] text-[48px] font-bold">
                 secure contribution
               </h1>
@@ -80,12 +87,11 @@ function ServicePage() {
             </div>
             <div className="w-[646px] h-[388px] grid justify-between  text-left">
               <div className="flex gap-2">
-                <GiShakingHands className="size-15" />
-                <FaUserFriends className="size-15 text-secondary-300" />
+                <SlChart className="size-15 text-secondary-300" />
               </div>
 
               <h1 className="text-[#F4F4F4] text-[48px]  font-bold">
-                Join Existing GroupTrack & manage your Circle
+                Track & manage your Circle
               </h1>
               <span className="text-[#F4F4F4] text-[32px] font-poppins mb-20">
                 Stay on top of your savings with real time dashboards. View
@@ -98,31 +104,37 @@ function ServicePage() {
         <div className="grid grid-cols-2 gap-10 pt-10 ">
           <img
             className="w-full h-[461px] "
-            src="image/a1333eb4795af0a5477d96165b386faa1f4921f7.jpg"       
+            src="image/db8ca82f6fb9ef4a4ed0da74a73fea1d2022c46e.jpg"
             alt=""
           />
           <img
             className="w-full h-[461px]"
-            src="image/9be5ea9f89818e29af05a4a31e94a933c1cfd2d9.jpg"
+            src="image/a1333eb4795af0a5477d96165b386faa1f4921f7.jpg"
             alt=""
           />
         </div>
         <div className="w-full h-[388px]">
           <div className="grid grid-cols-2 gap-10 pt-10 ">
             <div className="w-[646px] h-[388px] grid justify-between  text-left">
-              <FaUserFriends className="size-15 text-secondary-300" />
+              <div className="flex gap-2">
+                <AiFillThunderbolt className="size-15 text-secondary-300" />
+                <IoIosNotificationsOutline className="size-15 text-secondary-300" />
+              </div>
               <h1 className="text-[#F4F4F4] text-[48px] font-bold">
                 Autometed Reminders
               </h1>
-              <span className="text-[#F4F4F4] text-[32px] font-poppins">
+              <span className="text-[#F4F4F4] text-[32px] font-poppins mb-20">
                 Never miss a payment. Get instant SMS or Email reminders when
                 it`s time to contribute or when your payout is coming up.
               </span>
             </div>
             <div className="w-[646px] h-[388px] grid justify-between  text-left">
-              <div className="flex gap-2">
-                <GiShakingHands className="size-15" />
-                <FaUserFriends className="size-15 text-secondary-300" />
+              <div className="flex gap-1 items-center">
+                <img
+                  src="image/2064495_education_graduation_learn_school_student_icon.svg"
+                  className="size-20"
+                />
+                <VscBook className="size-15 text-secondary-300" />
               </div>
 
               <h1 className="text-[#F4F4F4] text-[48px] font-bold">
@@ -133,6 +145,65 @@ function ServicePage() {
                 financial decision and maximize your savings potential{" "}
               </span>
             </div>
+          </div>
+        </div>
+        <div className="">
+          <h1 className="text-[96px]">why choose Aguka Services</h1>
+          <img
+            src="image/a37628210e9032f7556be6b22e02808bb3992ac1.jpg"
+            alt="why choose aguka"
+            className="w-[1592px] h-[972px] mt-4 ml-[68px] rounded-[30px]"
+          />
+          <div className="grid grid-cols-2 ">
+            <div className="w-100  mt-10 ml-[190px]">
+              <div className="flex  gap-2 items-center p-2 bg-[#344A46]">
+                <SquareCheckBig className="size-10 text-[#64DB86]" />
+                <span className="text-3xl font-bold font-poppins">
+                  Simple & transparent
+                </span>
+              </div>
+            </div>
+            <div className="w-100  mt-10 ">
+              <div className="flex  gap-2 items-center p-2 bg-[#344A46]">
+                <SquareCheckBig className="size-10 text-[#64DB86]" />
+                <span className="text-3xl font-bold font-poppins">
+                  Simple & transparent
+                </span>
+              </div>
+            </div>
+            <div className="w-150  mt-10 ml-[190px]">
+              <div className="flex  gap-2 items-center p-2 bg-[#344A46]">
+                <SquareCheckBig className="size-10 text-[#64DB86]" />
+                <span className="text-3xl font-bold font-poppins">
+                  Community_Powered Wealth Building
+                </span>
+              </div>
+            </div>
+          </div>
+          <img
+            src="image/887f3273bca97c5fb03f4741386d1416a39ef45a.jpg"
+            alt="why choose aguka"
+            className="w-[1592px] h-[972px] mt-4 ml-[68px] rounded-[30px]"
+          />
+          <div className="space-y-15 m-15">
+            <h1 className="text-4xl">Ready to start Saving Smarter?</h1>
+            <div className="flex gap-10 place-content-center text-3xl font-bold font-poppins ">
+              <button className="bg-[#F9A825] text-black p-4 rounded-2xl ">
+                Create your circle
+              </button>
+              <button className="bg-[#82807D] text-white p-4 rounded-2xl ">
+                Join a group
+              </button>
+            </div>
+            <div className="place-items-center">
+              <hr className="w-300" />
+            </div>
+          </div>
+          <div className="mt-20">
+            <span>
+              &copy; 2025 Aguka.all rights reserved.Building Wealth through
+              community.
+            </span>
           </div>
         </div>
       </div>
