6637d7965c7249692955febf5b25a6c8966a71d8
patricknambajimana <patricknambajimana6@gmail.com>
2025-09-17 10:12:48 +0200

    added

--- src/features/auth/FillBeforeRegister.tsx
+++ src/features/auth/FillBeforeRegister.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import logo from "assets/logo/agukalogo.png";
 import type { ResetPasswordForm, ResetPasswordErrors } from "types/auth";
 
