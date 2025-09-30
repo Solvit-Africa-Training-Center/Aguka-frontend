2c9c93b977299b0048a232c0389da7a1e55da1ec
patricknambajimana <patricknambajimana6@gmail.com>
2025-09-30 17:47:00 +0200

    added change on dashboard

--- src/features/dashboard/AdminDashboard.tsx
+++ src/features/dashboard/AdminDashboard.tsx
@@ -8,16 +8,17 @@ import RecentActivities from "@components/dashboard/admin/RecentActivities";
 import CurrentStatistics from "@components/dashboard/admin/CurrentStatics";
 import { useGetAdminOverviewQuery } from "@services/api/adminApi";
 
+
 const AdminDashboard = () => {
   const { data: overview, isLoading, isError } = useGetAdminOverviewQuery();
-
+  console.log(overview);
   const cards = [
     {
       title: "Total Users",
       icon: (
         <HiUsers className="w-11 h-11 bg-[#005159] p-2 rounded-full text-secondary-300" />
       ),
-      value: overview?.totalUsers ?? "--",
+      value: overview?.data.totalUsers ?? "--",
       subtitle: "Users in the system",
     },
     {
@@ -25,7 +26,7 @@ const AdminDashboard = () => {
       icon: (
         <TbWaveSawTool className="w-11 h-11 p-2 rounded-full text-[#006D75]" />
       ),
-      value: overview?.totalGroups ?? "--",
+      value: overview?.data.totalGroups ?? "--",
       subtitle: "Registered groups",
     },
     {
@@ -33,13 +34,15 @@ const AdminDashboard = () => {
       icon: (
         <LuShield className="w-11 h-11 p-2 rounded-full text-secondary-300" />
       ),
-      value: overview?.usersPerGroup?.length ?? "--",
+      value: overview?.data.usersPerGroup?.length ?? "--",
       subtitle: "Groups with users",
     },
     {
       title: "Group Presidents",
       icon: <GoAlert className="w-11 h-11 p-2 rounded-full text-[#E53935]" />,
-      value: overview?.groupPresidents?.length ?? "--",
+      value: overview?.data?.groupStats
+        ? overview.data.groupStats.filter((g) => g.president !== null).length
+        : "--",
       subtitle: "Presidents with contacts",
     },
   ];
